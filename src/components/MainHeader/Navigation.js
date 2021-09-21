3b6fec735bff4caea3c5d2683c56105863e4da77
Yaghoob Abdollahi <yaghoob.silvana14@gmail.com>
2021-09-21 11:38:40 +0430

    move state managing to auth-context

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useContext} from "react";
 import AuthContext from "../../store/auth-context";
+
 
 import classes from "./Navigation.module.css";
 
-const Navigation = (props) => {
+const Navigation = () => {
+  const ctx = useContext(AuthContext);
   return (
     <AuthContext.Consumer>
       {(ctx) => {
@@ -22,7 +24,7 @@ const Navigation = (props) => {
               )}
               {ctx.isLoggedIn && (
                 <li>
-                  <button onClick={props.onLogout}>Logout</button>
+                  <button onClick={ctx.onLogout}>Logout</button>
                 </li>
               )}
             </ul>
